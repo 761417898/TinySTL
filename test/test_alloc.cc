45ba97e777140e83c04ae3c80f3e1976a8efad90
gaohongzhi <761417898@qq.com>
2018-12-21 21:44:53 +0800

    add unittest

--- test/test_alloc.cc
+++ test/test_alloc.cc
@@ -1,11 +1,31 @@
 #include <gtest/gtest.h>
 #include "../src/alloc.h"
 
+#include <stdlib.h>
+#include <stdio.h>
+
 namespace tinystl {
     namespace test_alloc {
         TEST(allocate, allocate_0_bytes) {
             EXPECT_EQ(nullptr, alloc::allocate(size_t(0)));
         }
+
+        TEST(allocate, allocate_more_than_128_bytes) {
+            int *p = (int*)alloc::allocate(sizeof(int) * 30);
+            for (int i = 0; i < 30; ++i) {
+                p[i] = i;
+            }
+            for (int i = 0; i < 30; ++i) {
+                EXPECT_EQ(i, p[i]);
+            }
+        }
+
+        TEST(allocate, allocate_less_than_128_bytes) {
+            long long *p = (long long*)alloc::allocate(sizeof(long long));
+            *p = 761417898;
+            EXPECT_EQ(761417898, *p);
+        }
+
 
     }
 }
@@ -13,6 +33,8 @@ namespace tinystl {
 
 int main(int argc, char **argv)
 {
+
+
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
